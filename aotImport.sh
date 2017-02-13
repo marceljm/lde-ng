cb8710a633021260449ce9eb0460779188d033ee
marceljm <marceljmachado@gmail.com>
2017-02-13 15:46:21 -0200

    Removing unnecessary css

--- aotImport.sh
+++ aotImport.sh
@@ -1,7 +1,10 @@
-cp node_modules/core-js/client/shim.min.js aot
 cp node_modules/primeng/resources/primeng.min.css aot
-cp node_modules/primeng/resources/themes/omega/theme.css aot
+cp node_modules/core-js/client/shim.min.js aot
 cp node_modules/reflect-metadata/Reflect.js aot
 cp node_modules/systemjs/dist/system.src.js aot
 cp node_modules/zone.js/dist/zone.js aot
+rm -r aot/resources
 cp -r src/resources aot
+mkdir -p aot/node_modules/primeng/resources/themes/omega/fonts
+cp node_modules/primeng/resources/themes/omega/fonts/* aot/node_modules/primeng/resources/themes/omega/fonts
+cp node_modules/primeng/resources/themes/omega/theme.css aot/node_modules/primeng/resources/themes/omega
