eeefece3c6a8dbdbae91bd50ae61e2d2b7fcf0bb
marceljm <marceljmachado@gmail.com>
2017-02-13 14:01:59 -0200

    import script

---
+++ aotImport.sh
@@ -0,0 +1,7 @@
+cp node_modules/core-js/client/shim.min.js aot
+cp node_modules/primeng/resources/primeng.min.css aot
+cp node_modules/primeng/resources/themes/omega/theme.css aot
+cp node_modules/reflect-metadata/Reflect.js aot
+cp node_modules/systemjs/dist/system.src.js aot
+cp node_modules/zone.js/dist/zone.js aot
+cp -r src/resources aot
